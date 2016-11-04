ca1282958f8b4f3c5425ed71a7631491cd0d1e5c
apetrenko <apetrenko@lohika.com>
2016-11-04 13:18:31 +0200

    updateMarkup now updates axis and axis labels

--- src/BarChart.js
+++ src/BarChart.js
@@ -50,6 +50,7 @@ const BarChart = React.createClass({
         const root = new ReactFauxDom.Element('div');
         const faux = this.connectFauxDOM(root, 'chart');
 
+        // svg
         const svg = d3.select(faux)
             .append('svg')
             .attr('width', barData.width + barData.margin.left + barData.margin.right)
@@ -57,40 +58,48 @@ const BarChart = React.createClass({
             .append('g')
             .attr("transform", `translate(${barData.margin.left}, ${barData.margin.top})`);
 
+        // bar
         let rect = svg.selectAll('rect')
             .data(barData.data)
             .enter().append('rect')
-            .attr('height', 0)
-            .attr('opacity', 0.1)
+            .attr('height', 0) // 0 value for transition
             .attr('class', 'bar')
             .attr('x', d => barData.xScale(d[barData.xKey]))
             .attr('width', barData.xScale.bandwidth())
-            .attr('y', d => barData.yScale(0))
-            .attr('height', d => barData.height - barData.yScale(0));
+            .attr('y', d => barData.yScale(0)) // 0 value for transition
+            .attr('height', d => barData.height - barData.yScale(0)); // 0 value for transition
 
-
+        // tooltip
         rect.append('title')
             .text(d => `${d[barData.xKey]}: ${d[barData.yKey]}`);
 
+        // bar transition from 0 to action values
         rect.transition()
             .duration(1000)
-            .attr('opacity', 1)
             .attr('height', d => barData.height - barData.yScale(d[barData.yKey]))
             .attr('y', d => barData.yScale(d[barData.yKey]));
 
+        // x axis
         svg.append('g')
+            .attr('class', 'axis x-axis')
             .attr('transform', `translate(0, ${barData.height})`)
             .call(d3.axisBottom(barData.xScale));
 
+        // x axis label
         svg.append('text')
+            .attr('class', 'axis-label x-axis-label')
             .attr('transform', `translate(${barData.width / 2}, ${barData.height + barData.margin.top + 20})`)
             .style('text-anchor', 'middle')
             .text(barData.xAxisLabel);
 
+        // y axis
         svg.append('g')
+            .attr('class', 'axis y-axis')
             .call(d3.axisLeft(barData.yScale));
 
+        // y axis label
         svg.append('text')
+            .attr('class', 'axis-label y-axis-label')
             .attr('transform', 'rotate(-90)')
             .attr('x', 0 - (barData.height / 2))
             .attr('y', 10 - barData.margin.left)
@@ -101,14 +110,42 @@ const BarChart = React.createClass({
         this.animateFauxDOM(1000);
     },
     updateMarkup(config) {
+        const duration = 1000;
         const barData = this.getDataFromConfig(config);
+
+        // perform bar transition from previous values to new values
         d3.selectAll('.bar')
             .data(barData.data)
             .transition()
-            .duration(1000)
-            .attr('opacity', 1)
+            .duration(duration)
             .attr('height', d => barData.height - barData.yScale(d[barData.yKey]))
             .attr('y', d => barData.yScale(d[barData.yKey]));
+
+        // x-axis update
+        d3.select('.x-axis')
+            .transition()
+            .duration(duration)
+            .call(d3.axisBottom(barData.xScale));
+
+        d3.select('.x-axis-label')
+            .attr('opacity', 0)
+            .transition()
+            .duration(duration)
+            .attr('opacity', 1)
+            .text(barData.xAxisLabel);
+
+        // y-axis update
+        d3.select('.y-axis')
+            .transition()
+            .duration(duration)
+            .call(d3.axisLeft(barData.yScale));
+
+        d3.select('.y-axis-label')
+            .attr('opacity', 0)
+            .transition()
+            .duration(duration)
+            .attr('opacity', 1)
+            .text(barData.yAxisLabel);
     },
 
     render() {
