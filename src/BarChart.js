bccbc74abe53118cd20f7f33b9abebd94cc0eb37
apetrenko <apetrenko@lohika.com>
2016-11-04 12:27:16 +0200

    1. Separate generateMarkup into generateInitialMarkup and updateMarkup, 2. refactor a function to extract data from config and generate common data for both generateInitialMarkup and updateMarkup

--- src/BarChart.js
+++ src/BarChart.js
@@ -11,80 +11,104 @@ const BarChart = React.createClass({
     getInitialState() {
         return {};
     },
-    componentWillReceiveProps(nextProps) {
-        this.generateMarkup(nextProps.config);
-    },
     componentDidMount() {
-        this.generateMarkup(this.props.config);
+        this.generateInitialMarkup(this.props.config);
     },
-    generateMarkup(config) {
+    componentWillReceiveProps(nextProps) {
+        this.updateMarkup(nextProps.config);
+    },
+    getDataFromConfig(config) {
         const {xKey, yKey, data, xAxisLabel, yAxisLabel} = config;
         const margin = {top: 20, right: 20, bottom: 50, left: 50},
             width = this.props.width - margin.left - margin.right,
             height = this.props.height - margin.top - margin.bottom;
 
-        const root = new ReactFauxDom.Element('div');
-        const faux = this.connectFauxDOM(root, 'chart');
-        const component = this;
-
-        const svg = d3.select(faux)
-            .append('svg')
-            .attr('width', width + margin.left + margin.right)
-            .attr('height', height + margin.top + margin.bottom)
-            .append('g')
-            .attr("transform", `translate(${margin.left}, ${margin.top})`);
-
-        const x = d3.scaleBand()
+        const xScale = d3.scaleBand()
             .domain(data.map(d => d[xKey]))
             .range([0, width])
             .padding(0.1);
 
-        const y = d3.scaleLinear()
+        const yScale = d3.scaleLinear()
             .domain([0, d3.max(data, d => d[yKey])])
             .range([height, 0]);
 
+        return {
+            xKey,
+            yKey,
+            data,
+            xAxisLabel,
+            yAxisLabel,
+            margin,
+            width,
+            height,
+            xScale,
+            yScale
+        }
+    },
+    generateInitialMarkup(config) {
+        var barData = this.getDataFromConfig(config);
+        const root = new ReactFauxDom.Element('div');
+        const faux = this.connectFauxDOM(root, 'chart');
+
+        const svg = d3.select(faux)
+            .append('svg')
+            .attr('width', barData.width + barData.margin.left + barData.margin.right)
+            .attr('height', barData.height + barData.margin.top + barData.margin.bottom)
+            .append('g')
+            .attr("transform", `translate(${barData.margin.left}, ${barData.margin.top})`);
+
         let rect = svg.selectAll('rect')
-            .data(data)
+            .data(barData.data)
             .enter().append('rect')
             .attr('height', 0)
             .attr('opacity', 0.1)
             .attr('class', 'bar')
-            .attr('x', d => x(d[xKey]))
-            .attr('width', x.bandwidth())
-            .attr('y', d => y(0))
-            .attr('height', d => height - y(0));
+            .attr('x', d => barData.xScale(d[barData.xKey]))
+            .attr('width', barData.xScale.bandwidth())
+            .attr('y', d => barData.yScale(0))
+            .attr('height', d => barData.height - barData.yScale(0));
 
 
         rect.append('title')
-            .text(d => `${d[xKey]}: ${d[yKey]}`);
+            .text(d => `${d[barData.xKey]}: ${d[barData.yKey]}`);
 
         rect.transition()
             .duration(1000)
             .attr('opacity', 1)
-            .attr('height', d => height - y(d[yKey]))
-            .attr('y', d => y(d[yKey]));
+            .attr('height', d => barData.height - barData.yScale(d[barData.yKey]))
+            .attr('y', d => barData.yScale(d[barData.yKey]));
 
         svg.append('g')
-            .attr('transform', `translate(0, ${height})`)
-            .call(d3.axisBottom(x));
+            .attr('transform', `translate(0, ${barData.height})`)
+            .call(d3.axisBottom(barData.xScale));
 
         svg.append('text')
-            .attr('transform', `translate(${width / 2}, ${height + margin.top + 20})`)
+            .attr('transform', `translate(${barData.width / 2}, ${barData.height + barData.margin.top + 20})`)
             .style('text-anchor', 'middle')
-            .text(xAxisLabel);
+            .text(barData.xAxisLabel);
 
         svg.append('g')
-            .call(d3.axisLeft(y));
+            .call(d3.axisLeft(barData.yScale));
 
         svg.append('text')
             .attr('transform', 'rotate(-90)')
-            .attr('x', 0 - (height / 2))
-            .attr('y', 10 - margin.left)
+            .attr('x', 0 - (barData.height / 2))
+            .attr('y', 10 - barData.margin.left)
             .attr('dy', '1em')
             .attr('text-anchor', 'middle')
-            .text(yAxisLabel);
+            .text(barData.yAxisLabel);
 
-        component.animateFauxDOM(1000);
+        this.animateFauxDOM(1000);
+    },
+    updateMarkup(config) {
+        const barData = this.getDataFromConfig(config);
+        d3.selectAll('.bar')
+            .data(barData.data)
+            .transition()
+            .duration(1000)
+            .attr('opacity', 1)
+            .attr('height', d => barData.height - barData.yScale(d[barData.yKey]))
+            .attr('y', d => barData.yScale(d[barData.yKey]));
     },
 
     render() {
